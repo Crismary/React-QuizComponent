ddc1ccd98f0741683d3ff96c57817c88e3d7f220
Goncalves <crismary.goncalves@accenture.com>
2020-07-20 11:08:25 +0100

    up to 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,22 +2,5 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-Class Quiz extends React.Component {
-  constructor(props){
-    super(props);
-
-  }
-
-  render() {
-    return(
-      <div>
-        <div className="QuizQuestion">
-
-        </div>
-      </div>
-    );
-  }
-}
-
 
 export default Quiz;
