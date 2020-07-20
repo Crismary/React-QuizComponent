d88a1808c612b94462b03eef04663219dd6ed898
Goncalves <crismary.goncalves@accenture.com>
2020-07-20 11:06:40 +0100

    up to 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ let quizData = require('./quiz_data.json')
 Class Quiz extends React.Component {
   constructor(props){
     super(props);
-    this.state = {quiz_position: 1};
+
   }
 
   render() {
